24601a837d7b36f8055b55b60b0ae7e0826da9d4
Vaishakh <flyingsakura4@gmail.com>
2026-02-17 18:15:10 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the app</h1>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>Login</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+        <UserButton />
+      </SignedIn>
+    </>
+  );
+}
+
+export default App;
